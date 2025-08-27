d5a83f5194a74d0cad75a31730197dc7b9848c93
Armaan Bhatoya <armaanbhatoya390@gmail.com>
2025-08-27 22:02:41 +0530

    added_file

---
+++ quick_sort.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int partition(vector<int> &arr, int low, int high){
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+    while(i < j){
+        while(arr[i] <= pivot && i < high) i++;
+        while(arr[j] >= pivot && j > low) j--;
+        if(i < j) swap(arr[i], arr[j]);
+    }
+    swap(arr[low], arr[j]);
+    return j; 
+
+}
+
+void quick_sort(vector<int> &arr, int low, int high){
+    if(low < high){
+        int partiton_index = partition(arr, low, high);
+        quick_sort(arr, low, partiton_index - 1);
+        quick_sort(arr, partiton_index + 1, high);
+    }
+    
+}
+
+int main(){
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    vector<int> arr(n);
+    cout << "Enter " << n << " elements: ";
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    quick_sort(arr, 0, n - 1);
+    cout << "Sorted array: ";
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
