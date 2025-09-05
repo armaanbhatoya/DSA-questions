be789036915a06c9faaf5ff5ba15be18807458c2
Armaan Bhatoya <armaanbhatoya390@gmail.com>
2025-09-05 20:05:38 +0530

    edited

--- linked_list.cpp
+++ linked_list.cpp
@@ -18,9 +18,20 @@ class Node{
 };
 
 
+Node* convertarr2ll(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i = 1; i<arr.size(); i++){
+        Node* temp = new Node(arr[i]);              //head
+        mover->next = temp;                        //mover      temp
+        mover = temp;
+    }
+    return head;
+}
+
 int main(){
     vector<int> arr= {2,3,4,6};
-    Node* y = new Node (arr[0]);// ********
-    cout<< y->data<<endl;
+    Node* head = convertarr2ll(arr);
+    cout<<head->data<<endl;
     return 0;
 }
