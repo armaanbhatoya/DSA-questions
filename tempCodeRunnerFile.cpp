ca938f9a549a46b533a8b9e4cc8e21cd2b8b08c4
Armaan Bhatoya <armaanbhatoya390@gmail.com>
2025-10-06 09:57:12 +0530

    edits

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,5 +1,28 @@
-head = deletebyposition(head, 3);
-    // Node_traversal(head);
+#include<iostream>
+using namespace std;
 
-    // head = deletebyvalue(head, 6);
-    // Node_traversal(head);
+class box{
+    double l,w,h;
+    public:
+        box& setl(double l){
+            this->l = l;
+            return *this;
+        }
+        box& setw(double w){
+            this->w = w;
+            return *this;
+        }
+        box& seth(double h){
+            this->h = h;
+            return *this;
+        }
+        void get_data(){
+            cout<<l<<endl<<w<<endl<<h<<endl;
+        }
+};
+int main(){
+    box a,b,c;
+    a.setl(2).setw(3).seth(5);
+    a.get_data();
+    return 0;
+}
