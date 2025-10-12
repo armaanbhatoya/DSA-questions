23177403b2b44ee247f70a96cdcdb15f8a4eaa33
Armaan Bhatoya <armaanbhatoya390@gmail.com>
2025-10-12 16:23:07 +0530

    trees new file

---
+++ trees/representaion.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    Node(int v){
+        data = v;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+int main(){
+    struct Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    
+    root->left->right = new Node(5);
+    return 0;
+}
