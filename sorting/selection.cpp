0e79a842404122b29a759b29934280c00000570f
Armaan Bhatoya <armaanbhatoya390@gmail.com>
2025-12-22 16:51:24 +0530

    modified

---
+++ sorting/selection.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void selection_sort(int arr[], int n){
+    for (int i = 0; i < n-1; i++)
+    {
+        int mini = i;
+        for (int j = i; j < n; j++) 
+        {
+            if(arr[j]<arr[mini]) mini = j;
+        }
+        int temp = arr[mini];
+        arr[mini] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+int main(){
+    int n;
+    cout<<"enter the size of the array : ";
+    cin>>n;
+    int arr[n];
+    cout<<"enter the elements of the array : ";
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+
+
+    selection_sort(arr,n);
+
+    cout<<"After the selection sort---->"<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<< " ";
+    }
+    cout<<endl;
+
+    
+}
