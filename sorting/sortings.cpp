7d46a7f84ebccb9a7d093273bef1546989fc4d24
Armaan Bhatoya <armaanbhatoya390@gmail.com>
2025-12-26 16:51:44 +0530

    added quick sort functions

--- sorting/sortings.cpp
+++ sorting/sortings.cpp
@@ -68,6 +68,28 @@ void merge_sort(int arr[], int low, int high){
     merge(arr,low,mid,high);
 }
 
+int findp(int arr[], int low, int high){
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+    while (i<j)
+    {
+       while(arr[i]<=pivot && i <= high-1) i++;       
+       while(arr[j]>pivot && j >= low+1) j--;    
+       if(i<j) swap(arr[i],arr[j]);   
+    }
+    swap(arr[low], arr[j]);
+    return j;
+}
+
+void quick_sort(int arr[], int low, int high){
+    if(low < high){
+        int partition = findp(arr,low,high );
+        quick_sort(arr, low, partition - 1);
+        quick_sort(arr, partition + 1, high);
+    }
+}
+
 int main(){
     int n;
     cout<<"enter the size of the array : ";
@@ -84,7 +106,8 @@ int main(){
     // bubble_sort(arr,n);
     // insertion_sort(arr,n);
 
-    merge_sort(arr,0,n-1);
+    // merge_sort(arr,0,n-1);
+    quick_sort(arr,0,n-1);
 
     cout<<"After the  sort---->"<<endl;
     for (int i = 0; i < n; i++)
